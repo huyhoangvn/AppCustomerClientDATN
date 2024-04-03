40d2ae07fa9ee2d68c75fe03b34e0395f1473f1e
ductxph29059 <137851512+ductxph29059-herry@users.noreply.github.com>
2024-04-03 20:35:58 +0700

    crud-thông-tin-cá-nhân

--- src/screen/khachhang/ChangeMatKhauScreen.tsx
+++ src/screen/khachhang/ChangeMatKhauScreen.tsx
@@ -1,25 +1,122 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
 import NavProps from '../../models/props/NavProps';
+import {
+  faShop,
+  faPhone,
+  faLocationDot,
+  faEnvelope,
+  faLock,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+import {appColors} from '../../constants/appColors';
+import EditTextComponent from '../../component/EditTextComponent';
+import ButtonComponent from '../../component/ButtonComponent';
+import {getData} from '../../utils/storageUtils';
+import AlertComponent from '../../component/AlertComponent';
+import LoadingComponent from '../../component/LoadingComponent';
+import authenticationAPI from '../../apis/authApi';
+const ChangeMatKhauScreen: React.FC<NavProps> = ({navigation}) => {
+  const [id, setId] = useState('');
+  const [oldPass, setOldPass] = useState('');
+  const [userName, setUserName] = useState('');
+  const [newPass, setNewPass] = useState('');
+  const [reNewPass, setReNewPass] = useState('');
+  const [isChecked, setChecked] = useState<boolean>();
+  const [loading, setLoading] = useState<boolean>();
+  const [showAlert, setShowAlert] = useState(false);
+  const [msg, setMsg] = useState('');
+ 
+  const handleShowAlert = () => {
+    setShowAlert(true);
+  };
 
-const ChangeMatKhauScreen : React.FC<NavProps> = ({ navigation }) =>  {
+  const handleCloseAlert = () => {
+    setShowAlert(false);
+  };
+  const validateInputs = () => {
+    if (!newPass.trim()) {
+      return 'Vui lòng nhập mật khẩu';
+    }
+
+    if (reNewPass.trim() !== newPass.trim()) {
+      return 'Mật khẩu nhập lại không khớp với mật khẩu ban đầu';
+    }
+
+    return null;
+  };
+
+
+ 
+ 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Trang Chủ</Text>
+      <View style={styles.main}>
+        <EditTextComponent
+          label="text"
+          placeholder="Email"
+          value={userName}
+          iconColor="gray"
+          onChangeText={setUserName}
+          icon={faUser}
+        />
+
+        <EditTextComponent
+          label="text"
+          placeholder="Mật khẩu cũ"
+          value={oldPass}
+          iconColor="gray"
+          onChangeText={setOldPass}
+          icon={faLock}
+        />
+
+        <EditTextComponent
+          label="pass"
+          placeholder="Nhập mật khẩu mới"
+          value={newPass}
+          iconColor="gray"
+          onChangeText={setNewPass}
+          icon={faLock}
+        />
+
+        <EditTextComponent
+          label="pass"
+          placeholder="Nhập lại mật khẩu mới"
+          value={reNewPass}
+          iconColor="gray"
+          onChangeText={setReNewPass}
+          icon={faLock}
+        />
+        <ButtonComponent
+          type="primary"
+          text="Lưu"
+          textStyles={{color: 'white', fontSize: 20, fontWeight: 'bold'}}
+          // onPress={handelSave}
+        />
+      </View>
+      <AlertComponent
+        visible={showAlert}
+        message={msg}
+        onClose={handleCloseAlert}
+      />
+      <LoadingComponent visible={loading ?? false} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'white'
+    height: hp(50),
+    backgroundColor: appColors.white,
   },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
+
+  main: {
+    height: hp(50),
+    justifyContent: 'space-between',
   },
 });
 
--- src/screen/khachhang/DetailKhachHangScreen.tsx
+++ src/screen/khachhang/DetailKhachHangScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 import NavProps from '../../models/props/NavProps';
 import { ScrollView } from 'react-native-gesture-handler';
 import MoreDetail from '../../component/detail/MoreDetail';
@@ -7,60 +7,165 @@ import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faKey, faEdit } from '@fortawesome/free-solid-svg-icons';
 import { appColors } from '../../constants/appColors';
 import { getData } from '../../utils/storageUtils';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+import { appImageSize } from '../../constants/appImageSize';
 
-const KhachHangDetailScreen : React.FC<NavProps> = ({ navigation }) =>  {
-  useEffect(() => {
 
-  })
+const dummyData = {
+  tenNV: "TRAN XUAN DUC",
+  gioiTinh: 2, // 2 for male, 1 for female
+  taiKhoan: "ductxph29059@gmail.com",
+  diaChi: "Vân canh, Hoài Đức, Nam Từ Liêm ",
+  sdt: "0123456789",
+  hinhAnh: "https://cdn-i.vtcnews.vn/files/ctv.giaoduc/2019/03/15/img-1329-1-0135095.jpg" // URL of the image
+};
+
+const KhachHangDetailScreen: React.FC<NavProps> = ({ navigation }) =>  {
+
+  const [item, setItem] = useState<any>(dummyData);
+  const [msg, setMsg] = useState('');
+  const [loading, setLoading] = useState(false);
 
   const openDoiMatKhauScreen = () => {
-    //Truyền thông tin gì đó
     navigation.navigate("ChangeMatKhauScreen");
   };
 
   const openEditDetailKhachHangScreen = () => {
-    //Truyền thông tin gì đó
-    navigation.navigate("ChangeMatKhauScreen");
+    navigation.navigate("EditDetailKhachHangScreen");
+  };
+  const getDetail = async () => {
+    try {
+      setLoading(true);
+      const res = await authenticationAPI.HandleAuthentication(
+        `/nhanvien/nhanvienquanly/chi-tiet-nhan-vien/${idUser}`,
+        'get',
+      );
+
+      if (res.success === true) {
+        setItem(res.index);
+      } else {
+        // Xử lý khi có lỗi từ API
+        setMsg('Request failed. Please try again.');
+        handleShowAlert();
+      }
+    } catch (err) {
+      console.log(err);
+      setMsg('Request timeout. Please try again later.');
+      handleShowAlert();
+    } finally {
+      setLoading(false);
+    }
   };
 
+  useEffect(() => {
+    getDetail();
+  }, []);
+
   return (
-      <ScrollView>
-          <View style={styles.container}>
-            <MoreDetail
-                title="Đổi mật khẩu" 
-                moreText="Xem thêm"
-                onPress={() => openDoiMatKhauScreen()} 
-                icon={faKey} // Icon tùy chọn
-                iconColor={appColors.primary}
-                titleColor={appColors.primary}
-                moreTextColor ={appColors.primary}
-                showTopBorder={false}
-                boldTitle={false}
-            />
-            <MoreDetail
-                title="Sửa thông tin" 
-                moreText="Xem thêm"
-                onPress={() => openEditDetailKhachHangScreen()} 
-                icon={faEdit} // Icon tùy chọn
-                iconColor={appColors.primary}
-                titleColor={appColors.primary}
-                moreTextColor ={appColors.primary}
-                showTopBorder={false}
-                boldTitle={false}
-            /> 
-          </View> 
-      </ScrollView>
+    <ScrollView>
+      <View style={styles.container}>
+        <View style={styles.header}>
+        <Image
+            source={
+            (!item.hinhAnh || item.hinhAnh === "N/A") ?
+              require('./../../assest/image/default-avatar.jpg') :
+              { uri: item.hinhAnh }}
+            style={{ width: appImageSize.size100.width, height: appImageSize.size100.height }}
+            defaultSource={require('./../../assest/image/default-avatar.jpg')}
+          />  
+        </View>
+        <View style={styles.main}>
+          <View style={styles.viewText}>
+            <Text>Tên</Text>
+            <Text style={styles.textPrimary}>{item?.tenNV}</Text>
+          </View>
+          <View style={styles.viewText}>
+            <Text>Giới tính</Text>
+            <Text style={styles.textPrimary}>
+              {item?.gioiTinh === 2 ? 'Nam' : 'Nữ'}
+            </Text>
+          </View>
+          <View style={styles.viewText}>
+            <Text>Email</Text>
+            <Text style={styles.textPrimary}>{item?.taiKhoan}</Text>
+          </View>
+          <View style={styles.viewText}>
+            <Text>Địa chỉ</Text>
+            <Text
+              style={[styles.textPrimary, styles.wrapText, styles.addressText]}>
+              {item?.diaChi}
+            </Text>
+          </View>
+          <View style={styles.viewText}>
+            <Text>Số điện thoại</Text>
+            <Text style={styles.textPrimary}>{item?.sdt}</Text>
+          </View>
+        </View>
+
+        <MoreDetail
+          title="Đổi mật khẩu" 
+          moreText="Xem thêm"
+          onPress={() => openDoiMatKhauScreen()} 
+          icon={faKey} 
+          iconColor={appColors.primary}
+          titleColor={appColors.primary}
+          moreTextColor ={appColors.primary}
+          showTopBorder={false}
+          boldTitle={false}
+        />
+        <MoreDetail
+          title="Sửa thông tin" 
+          moreText="Xem thêm"
+          onPress={() => openEditDetailKhachHangScreen()} 
+          icon={faEdit} 
+          iconColor={appColors.primary}
+          titleColor={appColors.primary}
+          moreTextColor ={appColors.primary}
+          showTopBorder={false}
+          boldTitle={false}
+        /> 
+      </View> 
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: appColors.white,
+    justifyContent: 'space-between',
+  },
+  header: {
+    height: hp(20),
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'white',
-    padding: 10
-  }
+  },
+  main: {
+    paddingHorizontal: 10,
+    height: hp(40),
+    justifyContent: 'space-between',
+  },
+  viewText: {
+    flexDirection: 'row',
+    borderBottomWidth: 0.5,
+    paddingHorizontal: 5,
+    paddingVertical: 5,
+    justifyContent: 'space-between',
+  },
+  textPrimary: {
+    color: 'black',
+    fontWeight: 'bold',
+  },
+  wrapText: {
+    flexWrap: 'wrap',
+    textAlign: 'right',
+  },
+  addressText: {
+    width: '70%',
+  },
 });
 
 export default KhachHangDetailScreen;
