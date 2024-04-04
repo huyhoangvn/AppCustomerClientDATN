09d65feeabdb545e37604cfd2a99bea13114b1d4
ductxph29059 <137851512+ductxph29059-herry@users.noreply.github.com>
2024-04-04 17:11:29 +0700

    Crud-thong-tin-ca-nhan

--- src/screen/khachhang/DetailKhachHangScreen.tsx
+++ src/screen/khachhang/DetailKhachHangScreen.tsx
@@ -12,23 +12,38 @@ import {
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
 import { appImageSize } from '../../constants/appImageSize';
+import { formatCurrency } from '../../utils/currencyFormatUtils';
+import TextViewComponent from '../../component/text/TextViewComponent';
+
 
 
 const dummyData = {
-  tenNV: "TRAN XUAN DUC",
-  gioiTinh: 2, // 2 for male, 1 for female
-  taiKhoan: "ductxph29059@gmail.com",
-  diaChi: "Vân canh, Hoài Đức, Nam Từ Liêm ",
-  sdt: "0123456789",
-  hinhAnh: "https://cdn-i.vtcnews.vn/files/ctv.giaoduc/2019/03/15/img-1329-1-0135095.jpg" // URL of the image
-};
+  success: true,
+  index: 
+    {
+      tenKH: "TRAN XUAN DUC",
+      gioiTinh: 2, // 2 for male, 1 for female
+      taiKhoan: "ductxph29059@gmail.com",
+      diaChi: "Vân canh, Hoài Đức, Nam Từ Liêm ",
+      sdt: "0123456789",
+      hinhAnh: "https://cdn-i.vtcnews.vn/files/ctv.giaoduc/2019/03/15/img-1329-1-0135095.jpg" // URL of the image
+    },
+   
+  count: 3,
+  msg: ""
+}
 
 const KhachHangDetailScreen: React.FC<NavProps> = ({ navigation }) =>  {
 
+  const [tenKH, setTenKH] = useState('');
+  const [gioiTinh, setGioiTinh] = useState('');
+  const [taiKhoan, setTaiKhoan] = useState('');
+  const [sdt, setSdt] = useState('');
+  const [diaChi, setDiaChi] = useState('');
   const [item, setItem] = useState<any>(dummyData);
   const [msg, setMsg] = useState('');
   const [loading, setLoading] = useState(false);
-
+  const [data , setData] = useState('');
   const openDoiMatKhauScreen = () => {
     navigation.navigate("ChangeMatKhauScreen");
   };
@@ -38,23 +53,25 @@ const KhachHangDetailScreen: React.FC<NavProps> = ({ navigation }) =>  {
   };
   const getDetail = async () => {
     try {
-      setLoading(true);
-      const res = await authenticationAPI.HandleAuthentication(
-        `/nhanvien/nhanvienquanly/chi-tiet-nhan-vien/${idUser}`,
-        'get',
-      );
-
+      // const res : any = await authenticationAPI.HandleAuthentication(
+      //   '/khachhang/khachhang' + "/" + id,
+      //   'get',
+      // );
+      const res : any = dummyData
+      console.log(res);
+     
       if (res.success === true) {
-        setItem(res.index);
-      } else {
-        // Xử lý khi có lỗi từ API
-        setMsg('Request failed. Please try again.');
-        handleShowAlert();
+        const { index } = res;
+        setTenKH(index.tenKH);
+        setGioiTinh(index.gioiTinh);
+        setTaiKhoan(index.taiKhoan); //
+        setSdt(index.sdt); //
+        setDiaChi(index.diaChi); //
+
+        console.log(index.tenKH)
       }
-    } catch (err) {
-      console.log(err);
-      setMsg('Request timeout. Please try again later.');
-      handleShowAlert();
+    } catch (e) {
+      console.log(e);
     } finally {
       setLoading(false);
     }
@@ -67,41 +84,57 @@ const KhachHangDetailScreen: React.FC<NavProps> = ({ navigation }) =>  {
   return (
     <ScrollView>
       <View style={styles.container}>
-        <View style={styles.header}>
+      <View style={styles.header}>
         <Image
             source={
             (!item.hinhAnh || item.hinhAnh === "N/A") ?
               require('./../../assest/image/default-avatar.jpg') :
               { uri: item.hinhAnh }}
-            style={{ width: appImageSize.size100.width, height: appImageSize.size100.height }}
+            style={{ width: appImageSize.size100.width, height: appImageSize.size100.height, borderRadius: 50 }}
             defaultSource={require('./../../assest/image/default-avatar.jpg')}
           />  
         </View>
         <View style={styles.main}>
           <View style={styles.viewText}>
-            <Text>Tên</Text>
-            <Text style={styles.textPrimary}>{item?.tenNV}</Text>
+          <TextViewComponent
+            leftText="Tên"
+            rightText={tenKH}
+            leftBold={true}
+            showBorderBottom={false}
+          />
           </View>
           <View style={styles.viewText}>
-            <Text>Giới tính</Text>
-            <Text style={styles.textPrimary}>
-              {item?.gioiTinh === 2 ? 'Nam' : 'Nữ'}
-            </Text>
+          <TextViewComponent
+            leftText="Giới tính"
+            rightText=   {item?.gioiTinh === 2 ? 'Nam' : 'Nữ'}
+            leftBold={true}
+            showBorderBottom={false}
+          />         
           </View>
           <View style={styles.viewText}>
-            <Text>Email</Text>
-            <Text style={styles.textPrimary}>{item?.taiKhoan}</Text>
+          <TextViewComponent
+            leftText="Email"
+            rightText=  {taiKhoan}
+            leftBold={true}
+            showBorderBottom={false}
+          /> 
           </View>
           <View style={styles.viewText}>
-            <Text>Địa chỉ</Text>
-            <Text
-              style={[styles.textPrimary, styles.wrapText, styles.addressText]}>
-              {item?.diaChi}
-            </Text>
+          <TextViewComponent
+            leftText="Địa chỉ"
+            rightText=  {diaChi}
+            leftBold={true}
+            showBorderBottom={false}
+          /> 
+           
           </View>
           <View style={styles.viewText}>
-            <Text>Số điện thoại</Text>
-            <Text style={styles.textPrimary}>{item?.sdt}</Text>
+          <TextViewComponent
+            leftText="Số điện thoại"
+            rightText=  {sdt}
+            leftBold={true}
+            showBorderBottom={false}
+          />       
           </View>
         </View>
 
@@ -137,15 +170,14 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: appColors.white,
     justifyContent: 'space-between',
+    paddingHorizontal: 10,
   },
   header: {
-    height: hp(20),
+    height: hp(15),
     justifyContent: 'center',
     alignItems: 'center',
   },
   main: {
-    paddingHorizontal: 10,
-    height: hp(40),
     justifyContent: 'space-between',
   },
   viewText: {
@@ -155,17 +187,8 @@ const styles = StyleSheet.create({
     paddingVertical: 5,
     justifyContent: 'space-between',
   },
-  textPrimary: {
-    color: 'black',
-    fontWeight: 'bold',
-  },
-  wrapText: {
-    flexWrap: 'wrap',
-    textAlign: 'right',
-  },
-  addressText: {
-    width: '70%',
-  },
+ 
+ 
 });
 
 export default KhachHangDetailScreen;
