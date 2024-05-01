4f4d572b4d46498d4d1b43c4b3c59a2a3fc8e641
huyhoangvn <47599913+huyhoangvn@users.noreply.github.com>
2024-05-01 16:01:28 +0700

    sửa số lượng

--- src/component/text/QuantityComponent.tsx
+++ src/component/text/QuantityComponent.tsx
@@ -4,9 +4,11 @@ import { appFontSize } from '../../constants/appFontSizes';
 import { appColors } from '../../constants/appColors';
 
 const QuantityComponent = ({ label, soLuong }: any) => {
+  // Kiểm tra nếu soLuong là null thì gán giá trị là 0
+
   return (
     <Text style={{ marginVertical: 5, fontSize: appFontSize.normal, color: appColors.text }}>
-      {label} (<Text>{soLuong}</Text>)
+      {label} (<Text>{(soLuong)?soLuong:0}</Text>)
     </Text>
   );
 };
