7cc62f8368dcac46645cc25c39fc468590bc9c70
huyhoangvn <47599913+huyhoangvn@users.noreply.github.com>
2024-05-02 17:24:58 +0700

    build app

--- src/constants/appInfos.ts
+++ src/constants/appInfos.ts
@@ -1,5 +1,6 @@
 import {Dimensions} from 'react-native';
 
 export const appInfo = {
-  BASE_URL: 'http://172.20.10.3:3000',
+  BASE_URL: 'http://192.168.43.127:3000',
+  // BASE_URL: 'http://10.0.2.2:3000',
 };
